8b9fbc3fc6e01aff4a5be13415311882c897af53
Jarkko Saltiola <jarkko@saltiolacode.fi>
2024-08-18 19:38:47 +0300

    rename phel-test -> phel-wp-plugin

--- phel-wp-plugin.php
+++ phel-wp-plugin.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Phel demo plugin
+Plugin Name: Phel Demo Plugin
 Description: Adds admin widget printing some content with Phel
 Version: 0.1
 Author: Jarkko Saltiola
@@ -10,14 +10,13 @@ Author URI: https://codeberg.org/jasalt
 use Phel\Phel;
 
 $projectRootDir = __DIR__ . '/';
+error_log($projectRootDir);
 
 require $projectRootDir . 'vendor/autoload.php';
 
-global $projectRootDir;
-
 // HACK to avoid deprecation messages printed on page on phel startup
 ob_start();
-Phel::run($projectRootDir, 'phel-test\noop');
+Phel::run($projectRootDir, 'phel-wp-plugin\noop');
 ob_end_clean();
 
-Phel::run($projectRootDir, 'phel-test\main');
+Phel::run($projectRootDir, 'phel-wp-plugin\main');
