d08240ae4e0e756429d7ea51dfdeed532da9b12f
Jarkko Saltiola <jarkko@saltiolacode.fi>
2024-08-18 18:34:33 +0300

    increase xdebug.max_nesting_level
    for avoiding infinite loop detection false positive

--- phel-config.php
+++ phel-config.php
@@ -1,6 +1,9 @@
 <?php
 $projectRootDir = __DIR__ . '/';
 
+ini_set('xdebug.max_stack_frames', 300);
+ini_set('xdebug.max_nesting_level', 300);
+
 return (new \Phel\Config\PhelConfig())
     ->setErrorLogFile($projectRootDir . 'error.log')
     ->setSrcDirs(['src']);
