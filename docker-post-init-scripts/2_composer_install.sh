347f10170ebbaa02c06c45dd2effb0fac810dfd0
Chemaclass <chemaclass@outlook.es>
2024-11-17 17:47:52 +0100

    fix: 2_composer_install use local phel-lang if exists

--- docker-post-init-scripts/2_composer_install.sh
+++ docker-post-init-scripts/2_composer_install.sh
@@ -1,9 +1,20 @@
-cd /bitnami/wordpress/wp-content/plugins/phel-wp-plugin || exit
+#!/bin/bash
+
+PLUGIN_DIR="/bitnami/phel-wp-plugin"
+LOCAL_PHEL_LANG="/bitnami/phel-lang"
+VENDOR_PHEL_LANG="vendor/phel-lang"
+
+cd "$PLUGIN_DIR" || { echo "Failed to navigate to $PLUGIN_DIR. Exiting."; exit 1; }
+
 composer install --no-cache
 
-# if "local phel-lang" is not empty
-if [ -d "/bitnami/phel-lang" ] && [ "$(ls -A /bitnami/phel-lang 2>/dev/null)" ]; then
-  # override vendor phel-lang with symlink pointing to "local phel-lang"
-  rm -rf /bitnami/wordpress/wp-content/plugins/phel-wp-plugin/vendor/phel-lang/phel-lang
-  ln -s /bitnami/phel-lang /bitnami/wordpress/wp-content/plugins/phel-wp-plugin/vendor/phel-lang/phel-lang
+# Check if "local phel-lang" directory exists
+if [ -f "${LOCAL_PHEL_LANG}/bin/phel" ]; then
+  # Remove existing vendor phel-lang and create a symlink to local src
+  rm -rf "${VENDOR_PHEL_LANG}/phel-lang"
+  ln -s "$LOCAL_PHEL_LANG" "$VENDOR_PHEL_LANG"
+else
+  echo "No local phel found, keeping original from composer"
 fi
+
+echo "Phel version: " "$($PLUGIN_DIR/vendor/bin/phel -V)"
