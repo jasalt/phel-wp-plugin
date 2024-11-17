3195b98731e0ffb06055e9c754b7d4e50ec24afe
Chemaclass <chemaclass@outlook.es>
2024-11-17 13:08:48 +0100

    chore: enable linking phel-lang from local to vendor
    directly from docker-compose volumes

--- docker-post-init-scripts/2_composer_install.sh
+++ docker-post-init-scripts/2_composer_install.sh
@@ -1,2 +1,9 @@
-cd /bitnami/wordpress/wp-content/plugins/phel-wp-plugin
+cd /bitnami/wordpress/wp-content/plugins/phel-wp-plugin || exit
 composer install --no-cache
+
+# if "local phel-lang" is not empty
+if [ -d "/bitnami/phel-lang" ] && [ "$(ls -A /bitnami/phel-lang 2>/dev/null)" ]; then
+  # override vendor phel-lang with symlink pointing to "local phel-lang"
+  rm -rf /bitnami/wordpress/wp-content/plugins/phel-wp-plugin/vendor/phel-lang/phel-lang
+  ln -s /bitnami/phel-lang /bitnami/wordpress/wp-content/plugins/phel-wp-plugin/vendor/phel-lang/phel-lang
+fi
