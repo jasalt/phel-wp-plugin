2fd37dafb4e3670db0ecb2848b7ae9da120b2f07
Jarkko Saltiola <jarkko@saltiolacode.fi>
2025-08-01 12:28:30 +0300

    Disable notices
    Following notice started coming up for all Phel temp files during requests:
    
    tempnam(): file created in the system's temporary directory

--- phel-wp-plugin.php
+++ phel-wp-plugin.php
@@ -7,6 +7,10 @@ Author: Jarkko Saltiola
 Author URI: https://codeberg.org/jasalt
 */
 
+// Dismiss notice
+// PHP Notice:  tempnam(): file created in the system's temporary directory
+error_reporting(E_ALL & ~E_NOTICE);
+
 use Phel\Phel;
 
 $projectRootDir = __DIR__ . '/';
