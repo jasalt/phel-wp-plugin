cf04eb3fe559b63aa00c6000063751f66a1546b3
Jarkko Saltiola <jarkko@saltiolacode.fi>
2025-08-02 19:24:45 +0300

    Make sure there's write access to /tmp/phel

--- custom-entrypoint.sh
+++ custom-entrypoint.sh
@@ -31,6 +31,11 @@ if [ -f "/COMPOSE_INITIALIZED" ]; then
 else
 	echo "Running custom-entrypoint.sh initialization"
 
+	if [ ! -d "/tmp/phel" ]; then
+	    mkdir /tmp/phel
+	    chmod -R 777 /tmp/phel
+	fi
+
 	cd /var/www/html/wp-content/plugins/phel-wp-plugin
 	composer install
 
