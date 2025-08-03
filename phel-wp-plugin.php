a1ff865fb46f5481a8d665d2f73624d6795fddbb
Jarkko Saltiola <jarkko@saltiolacode.fi>
2025-08-03 21:25:47 +0300

    Avoid re-initializing Phel in REPL when requiring wp-load.php

--- phel-wp-plugin.php
+++ phel-wp-plugin.php
@@ -12,4 +12,13 @@ use Phel\Phel;
 $projectRootDir = __DIR__ . '/';
 require $projectRootDir . 'vendor/autoload.php';
 
-Phel::run($projectRootDir, 'phel-wp-plugin\main');
+if (isset($PHP_SELF) && $PHP_SELF !== "./vendor/bin/phel"){
+	// Initialize Phel environment in regular WP plugin context. This can be
+	// nalso narrowed to only specific routes or conditions to avoid it's
+	// runtime overhead where it's not needed.
+	Phel::run($projectRootDir, 'phel-wp-plugin\main');
+} else {
+	// Avoid re-initializing Phel environment during REPL session when requiring
+	// wp-load.php which initializes all plugins.
+	print("Running REPL, skip running plugin Phel::run \n");
+}
