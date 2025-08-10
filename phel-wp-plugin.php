33df4829690d7e80a9ed99df01b98ff61504d007
Jarkko Saltiola <jarkko@saltiolacode.fi>
2025-08-10 10:55:09 +0300

    WP-CLI command example

--- phel-wp-plugin.php
+++ phel-wp-plugin.php
@@ -13,12 +13,24 @@ $projectRootDir = __DIR__ . '/';
 require $projectRootDir . 'vendor/autoload.php';
 
 if (isset($PHP_SELF) && $PHP_SELF !== "./vendor/bin/phel"){
-	// Initialize Phel environment in regular WP plugin context. This can be
-	// nalso narrowed to only specific routes or conditions to avoid it's
-	// runtime overhead where it's not needed.
+	// Initialize Phel environment in regular WP plugin web request context.
+	// This can be also narrowed to only specific routes or conditions to avoid
+	// it's runtime overhead where it's not needed.
 	Phel::run($projectRootDir, 'phel-wp-plugin\main');
 } else {
-	// Avoid re-initializing Phel environment during REPL session when requiring
-	// wp-load.php which initializes all plugins.
-	print("Running REPL, skip running plugin Phel::run \n");
+	// Don't re-initialize Phel or run main namespace outside regular web request
+	// context e.g. when starting REPL session or running as WP-CLI command.
+	print("Skip running phel-wp-plugin\main outside web request context.\n");
 }
+
+
+/*
+ * Register WP-CLI command 'wp phel' running Phel namespace at `src/cli.phel`
+ * https://make.wordpress.org/cli/handbook/guides/commands-cookbook/
+ */
+WP_CLI::add_command( 'phel',
+					 function ( $args ){
+						 $projectRootDir = __DIR__ . '/';
+						 Phel::run($projectRootDir, 'phel-wp-plugin\cli');
+						 WP_CLI::success( "done!" );
+					 }, ['shortdesc' => 'Runs Phel code as WP-CLI command']);
