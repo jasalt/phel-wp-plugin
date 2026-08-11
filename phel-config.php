5bf52b1d442acc08bd41825b91c4881650f07d1e
Jarkko Saltiola <jarkko@saltiolacode.fi>
2026-08-11 17:00:47 +0300

    update phel dev-main

--- phel-config.php
+++ phel-config.php
@@ -6,12 +6,12 @@ ini_set('xdebug.max_stack_frames', 300);
 ini_set('xdebug.max_nesting_level', 300);
 
 return (new PhelConfig())
-    ->setSrcDirs(['src'])
-    ->setTestDirs(['tests'])
-    ->setVendorDir('vendor')
-    ->setErrorLogFile('data/error.log')
-    ->setIgnoreWhenBuilding(['src/local.phel'])
-    ->setNoCacheWhenBuilding([])
-    ->setKeepGeneratedTempFiles(false)
-	->setTempDir(sys_get_temp_dir().'/phel')
-    ->setFormatDirs(['src', 'tests']);
+    ->withSrcDirs(['src'])
+    ->withTestDirs(['tests'])
+    ->withVendorDir('vendor')
+    ->withErrorLogFile('data/error.log')
+    ->withIgnoreWhenBuilding(['src/local.phel'])
+    ->withNoCacheWhenBuilding([])
+    ->withKeepGeneratedTempFiles(false)
+    ->withTempDir(sys_get_temp_dir().'/phel')
+    ->withFormatDirs(['src', 'tests']);
